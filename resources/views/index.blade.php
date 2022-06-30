8ce7d532570f2127a225efb7e2fc1a3f48d1400a
Daniel Hollfelder <hollfelder_daniel@yahoo.de>
2022-07-01 01:42:36 +0200

    'bild'

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,7 +39,7 @@
     </div>
   </div>
 </nav>
-<img src="bilder/headerich.jpg" class="img-fluid" alt="Daniel Hollfelder">
+<img src="{{URL::to('public/images').'/headerich.jpg'}}" class="img-fluid" alt="Daniel Hollfelder">
     <h1>Hello, world!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </body>
